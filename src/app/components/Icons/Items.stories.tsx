9d9b4d153a1d896b703a4823a0c2d307f9e7589a
ChristianSeelemann <cseelemann@gmx.de>
2021-09-07 16:18:10 +0200

    ✨ Create items icon component

---
+++ src/app/components/Icons/Items.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Items from './Items';
+
+export default {
+  title: 'Component/Icons',
+  component: Items,
+};
+
+export const ItemsIcon = (): JSX.Element => <Items />;
