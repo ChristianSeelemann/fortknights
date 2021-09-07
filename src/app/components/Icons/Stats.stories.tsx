f863ddbbcdbe1a6b1932326e7bf004cad23b0b17
ChristianSeelemann <cseelemann@gmx.de>
2021-09-07 16:19:29 +0200

    ✨ Create stats icon component

---
+++ src/app/components/Icons/Stats.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Stats from './Stats';
+
+export default {
+  title: 'Component/Icons',
+  component: Stats,
+};
+
+export const StatsIcon = (): JSX.Element => <Stats />;
