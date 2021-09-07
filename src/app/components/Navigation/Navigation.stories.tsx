a033329da1cc0047f8d3377fae50bf7d4d200ac0
ChristianSeelemann <cseelemann@gmx.de>
2021-09-07 15:08:04 +0200

    ✨ Create navigation component for storybook

--- src/app/components/Navigation/Navigation.stories.tsx
+++ src/app/components/Navigation/Navigation.stories.tsx
@@ -6,5 +6,8 @@ export default {
   component: Navigation,
 };
 
-export const News = (): JSX.Element => <Navigation>Login</Navigation>;
-export const Items = (): JSX.Element => <Navigation>Register</Navigation>;
+export const News = (): JSX.Element => <Navigation active="news" />;
+export const Items = (): JSX.Element => <Navigation active="items" />;
+export const Stats = (): JSX.Element => <Navigation active="stats" />;
+export const Friends = (): JSX.Element => <Navigation active="friends" />;
+export const Map = (): JSX.Element => <Navigation active="map" />;
