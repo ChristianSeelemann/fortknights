4703d7fbb142edcf3b500ba2fe453fcba482c85e
ChristianSeelemann <cseelemann@gmx.de>
2021-09-08 18:20:01 +0200

    ✨ Add button component to storybook

--- src/app/components/Button/Button.stories.tsx
+++ src/app/components/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Friends from '../Icons/Friends';
 import Button from './Button';
 
 export default {
@@ -6,4 +7,30 @@ export default {
   component: Button,
 };
 
-export const Test = (): JSX.Element => <Button />;
+export const primary = (): JSX.Element => (
+  <Button style="primary" text="Primary" />
+);
+
+export const secondary = (): JSX.Element => (
+  <Button style="secondary" text="Secondary" />
+);
+
+export const accent = (): JSX.Element => (
+  <Button style="accent" text="Accent" />
+);
+
+export const warning = (): JSX.Element => (
+  <Button style="warning" text="Warning" />
+);
+
+export const success = (): JSX.Element => (
+  <Button style="success" text="Success" />
+);
+
+export const withIcon = (): JSX.Element => (
+  <Button
+    style="primary"
+    text="Success"
+    icon={<Friends color="var(--clr-white)" />}
+  />
+);
