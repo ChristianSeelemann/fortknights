fd48f7c191b23e5b84d8daf407949bc3e8d7f257
ChristianSeelemann <cseelemann@gmx.de>
2021-09-09 15:25:29 +0200

    ✨ Add tag and date props to storybook

--- src/app/components/Card/Card.stories.tsx
+++ src/app/components/Card/Card.stories.tsx
@@ -14,5 +14,7 @@ export const News = (): JSX.Element => (
     title="SCHAUT EUCH DIE FNCS FINALS VON KAPITEL 2 – SAISON 7 IN PARTY ROYALE AN"
     image="https://cdn2.unrealengine.com/Fortnite%2Fblog%2Fwhats-new-in-br-in-v11-20%2FBR07_News_Featured_Evergreens_v2_Pink-%281%29-1920x1080-a0e09ea34aafa903610a8f47c0e8386230d006ef.jpg"
     link="#"
+    date="23.08.21"
+    gamemode="br"
   />
 );
