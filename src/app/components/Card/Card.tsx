d1d402a780202844dd57035b21d896ff7ffdf942
ChristianSeelemann <cseelemann@gmx.de>
2021-09-09 15:25:15 +0200

    ✨ Add props for date and tag

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -7,17 +7,43 @@ interface CardProps {
   title: string;
   image: string;
   link: string;
+  date: string;
+  gamemode: 'br' | 'stw' | 'creative';
 }
 
-export default function Card({ title, image, link }: CardProps): JSX.Element {
+export default function Card({
+  title,
+  image,
+  link,
+  date,
+  gamemode,
+}: CardProps): JSX.Element {
+  let modeName;
+  if (gamemode === 'br') {
+    modeName = 'Battle Royale';
+  } else if (gamemode === 'stw') {
+    modeName = 'Save the World';
+  } else {
+    modeName = 'Creative';
+  }
+
+  let modeStyle;
+  if (gamemode === 'br') {
+    modeStyle = 'battleroyale';
+  } else if (gamemode === 'stw') {
+    modeStyle = 'savetheworld';
+  } else {
+    modeStyle = 'creative';
+  }
+
   return (
     <Link to={link}>
       <div className={styles.Card}>
         <img className={styles.CardImg} src={image} alt="News Image" />
         <div className={styles.CardOverlay}>
           <div className={styles.CardTags}>
-            <Tag text="Battle Royale" style="battleroyale" />
-            <Tag text="24.08.21" style="date"></Tag>
+            <Tag text={modeName} style={modeStyle} />
+            <Tag text={date} style="date"></Tag>
           </div>
           <div className={styles.CardTitle}>{title}</div>
         </div>
