52c9074f6f84864645950bd2c0d6335cf5944a5b
ChristianSeelemann <cseelemann@gmx.de>
2021-09-10 14:57:52 +0200

    ✨ Insert actual API data to news page

--- src/app/pages/News/News.tsx
+++ src/app/pages/News/News.tsx
@@ -1,36 +1,95 @@
 import React from 'react';
+import useFetch from '../../hooks/useFetch';
 import Card from '../../components/Card/Card';
 import Header from '../../components/Header/Header';
 import ToTop from '../../components/Icons/ToTop';
 import Navigation from '../../components/Navigation/Navigation';
-import Tag from '../../components/Tag/Tag';
 import styles from './News.module.css';
 
-interface NewsProps {
-  test?: string;
+interface NewsMotd {
+  data: {
+    motds: [
+      {
+        id: string;
+        title: string;
+        body: string;
+        image: string;
+      }
+    ];
+  };
 }
 
-export default function News({ test }: NewsProps): JSX.Element {
+interface NewsStw {
+  data: {
+    messages: [
+      {
+        title: string;
+        body: string;
+        image: string;
+      }
+    ];
+  };
+}
+
+export default function News(): JSX.Element {
+  const { data: battleRoyaleData } = useFetch<NewsMotd>(
+    'https://fortnite-api.com/v2/news/br'
+  );
+  const battleRoyaleNews = battleRoyaleData?.data.motds;
+
+  const { data: creativeData } = useFetch<NewsMotd>(
+    'https://fortnite-api.com/v2/news/creative'
+  );
+  const creativeNews = creativeData?.data.motds;
+
+  const { data: saveTheWorldData } = useFetch<NewsStw>(
+    'https://fortnite-api.com/v2/news/stw'
+  );
+  const saveTheWorldNews = saveTheWorldData?.data.messages;
+
   return (
     <section className={styles.news}>
-      <Header textThin="Fort" textBold="Knights" icon="notification" />
+      <Header textThin="Fort" textBold="Knights" icon="fortnite" />
       <main>
-        <section className={styles.news__tagGroup}>
-          <Tag style="battleroyale" text="Battle Royale" />
-          <Tag style="savetheworld" text="Save the World" />
-          <Tag style="creative" text="Creative" />
-        </section>
+        <h2>Battle Royale</h2>
         <section className={styles.news__itemGroup}>
-          <Card
-            title="Headline"
-            image="https://wallpaperaccess.com/full/3429710.jpg"
-            date="27.08.21"
-            gamemode="br"
-            link="#"
-          />
+          {battleRoyaleNews?.map((item) => (
+            <Card
+              key={item.id}
+              title={item.body}
+              image={item.image}
+              date={item.title}
+              gamemode="br"
+            />
+          ))}
+          <h2 className={styles.news__h2}>Save the World</h2>
+          {saveTheWorldNews?.map((item) => (
+            <Card
+              key={item.title}
+              title={item.body}
+              image={item.image}
+              date={item.title}
+              gamemode="stw"
+            />
+          ))}
+          <h2 className={styles.news__h2}>Creative</h2>
+          {creativeNews?.map((item) => (
+            <Card
+              key={item.id}
+              title={item.body}
+              image={item.image}
+              date={item.title}
+              gamemode="creative"
+            />
+          ))}
         </section>
         <div className={styles.news__toDo}>
           <ToTop color="var(--clr-white)" />
+          <span>
+            This is everything :)
+            <br />
+            Come back later for new great stuff!
+          </span>
         </div>
       </main>
       <Navigation active="news" />
