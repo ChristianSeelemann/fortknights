32be2e7796251fcb3466ef779c2a0b97398e6f83
ChristianSeelemann <cseelemann@gmx.de>
2021-09-10 12:38:10 +0200

    ♻️ Insert logic to scroll to top

--- src/app/components/Icons/ToTop.tsx
+++ src/app/components/Icons/ToTop.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 type toTopProps = {
   color: string;
-  onClick?: () => void;
 };
 
-export default function ToTop({ color, onClick }: toTopProps): JSX.Element {
+export default function ToTop({ color }: toTopProps): JSX.Element {
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <svg
       width="29"
@@ -13,7 +16,7 @@ export default function ToTop({ color, onClick }: toTopProps): JSX.Element {
       viewBox="0 0 29 32"
       fill={color}
       xmlns="http://www.w3.org/2000/svg"
-      onClick={onClick}
+      onClick={scrollTop}
     >
       <path d="M1.77778 21.3333H8.88889V32H19.5556V21.3333H26.6667L14.2222 7.11111L1.77778 21.3333ZM0 0H28.4444V3.55556H0V0Z" />
     </svg>
