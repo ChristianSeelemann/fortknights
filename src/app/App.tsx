105102e24414a1432b570d4c2ae80035987d46b6
ChristianSeelemann <cseelemann@gmx.de>
2021-09-10 10:12:42 +0200

    ♻️ Create path for home and root to news page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import News from './pages/News/News';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <header>
-        <p>Navigation</p>
-      </header>
-
       <Switch>
+        <Route path="/news">
+          <main>
+            <News />
+          </main>
+        </Route>
         <Route path="/">
-          <main>Home</main>
+          <main>
+            <News />
+          </main>
         </Route>
       </Switch>
     </BrowserRouter>
