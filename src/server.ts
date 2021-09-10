ae32e7d2bb87cb2b70d647ece237fc4816c5ffe0
ChristianSeelemann <cseelemann@gmx.de>
2021-09-10 16:14:06 +0200

    🐛 Revert changes couse of a bug

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,20 @@
 import dotenv from 'dotenv';
-import express from 'express';
 dotenv.config();
 
-const port = process.env.PORT || 3001;
+import express from 'express';
+
 const app = express();
+const port = process.env.PORT || 3001;
+
 app.use(express.json());
-app.disable('x-powered-by');
+
+app.get('/api', (_request, response) => {
+  response.send('Hello API');
+});
 
 app.use('/storybook', express.static('dist/storybook'));
-
-app.get('/api/', async (_req, res) => {
-  res.status(200).json({ message: 'API is running' });
-});
-
 app.use(express.static('dist/app'));
-app.get('*', (_request, response) => {
-  response.sendFile('index.html', { root: 'dist/app' });
-});
 
-app.listen(port, async () => {
-  console.log(`Listening at http://localhost:${port}`);
-  console.log(`Storybook is at http://localhost:${port}/storybook`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
 });
