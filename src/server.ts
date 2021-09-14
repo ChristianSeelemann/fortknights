558b4f3aa4b57f418e56d944ee5f91fa9b72ca50
ChristianSeelemann <cseelemann@gmx.de>
2021-09-14 12:17:05 +0200

    ✨ Add route for user data and stats

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
+import express from 'express';
+import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 dotenv.config();
-
-import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+
+app.get('/api/stats/:id', async (request, response) => {
+  const { id } = request.params;
+  const user = await fetch(
+    `https://fortnite-api.com/v2/stats/br/v2?name=${id}`
+  );
+  const userData = await user.json();
+  response.json(userData);
+});
 
 app.get('/api', (_request, response) => {
   response.send('Hello API');
