23e1b318864cea831ddb3018574e0a6b632cdd47
ChristianSeelemann <cseelemann@gmx.de>
2021-09-14 10:36:46 +0200

    🚚 Add friends route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Friends from './pages/Friends/Friends';
 import News from './pages/News/News';
 
 function App(): JSX.Element {
@@ -8,6 +9,9 @@ function App(): JSX.Element {
       <Switch>
         <Route path="/news">
           <News />
+        </Route>
+        <Route path="/friends">
+          <Friends />
         </Route>
         <Route path="/">
           <News />
