56dd033e2239a475b84ccf0c70c1147933173ad2
ChristianSeelemann <cseelemann@gmx.de>
2021-09-15 13:06:45 +0200

    ♻️ Refactor friendsFromAPI type

--- src/app/types/statsFromAPI.tsx
+++ src/app/types/statsFromAPI.tsx
@@ -1,58 +1,61 @@
 export default interface statsFromAPI {
-  result: boolean;
-  name: string;
-  account: {
-    level: number | null;
-    progress_pct: number | null;
+  data: {
+    result: boolean;
+    name: string;
+    account: {
+      level: number | null;
+      progress_pct: number | null;
+    };
+    global_stats: {
+      squad: {
+        placetop1: number;
+        kd: number;
+        winrate: number;
+        placetop3: number;
+        placetop5: number;
+        placetop6: number;
+        placetop10: number;
+        placetop12: number;
+        placetop25: number;
+        kills: number;
+        matchesplayed: number;
+        minutesplayed: number;
+        score: number;
+        playersoutlived: number;
+      };
+      duo: {
+        placetop1: number;
+        kd: number;
+        winrate: number;
+        placetop3: number;
+        placetop5: number;
+        placetop6: number;
+        placetop10: number;
+        placetop12: number;
+        placetop25: number;
+        kills: number;
+        matchesplayed: number;
+        minutesplayed: number;
+        score: number;
+        playersoutlived: number;
+      };
+      solo: {
+        placetop1: number;
+        kd: number;
+        winrate: number;
+        placetop3: number;
+        placetop5: number;
+        placetop6: number;
+        placetop10: number;
+        placetop12: number;
+        placetop25: number;
+        kills: number;
+        matchesplayed: number;
+        minutesplayed: number;
+        score: number;
+        playersoutlived: number;
+      };
+    };
   };
-  global_stats: {
-    squad: {
-      placetop1: number;
-      kd: number;
-      winrate: number;
-      placetop3: number;
-      placetop5: number;
-      placetop6: number;
-      placetop10: number;
-      placetop12: number;
-      placetop25: number;
-      kills: number;
-      matchesplayed: number;
-      minutesplayed: number;
-      score: number;
-      playersoutlived: number;
-    };
-    duo: {
-      placetop1: number;
-      kd: number;
-      winrate: number;
-      placetop3: number;
-      placetop5: number;
-      placetop6: number;
-      placetop10: number;
-      placetop12: number;
-      placetop25: number;
-      kills: number;
-      matchesplayed: number;
-      minutesplayed: number;
-      score: number;
-      playersoutlived: number;
-    };
-    solo: {
-      placetop1: number;
-      kd: number;
-      winrate: number;
-      placetop3: number;
-      placetop5: number;
-      placetop6: number;
-      placetop10: number;
-      placetop12: number;
-      placetop25: number;
-      kills: number;
-      matchesplayed: number;
-      minutesplayed: number;
-      score: number;
-      playersoutlived: number;
-    };
-  };
+  id: number;
 }
