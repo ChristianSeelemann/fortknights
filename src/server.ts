74ec16e9ea0dfcbc2a04a2e639b230ab507bb3d2
ChristianSeelemann <cseelemann@gmx.de>
2021-09-17 12:13:22 +0200

    ♻️ Swap API Key and get it from .env

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+const API_KEY = process.env.API_KEY;
 
 app.use(express.json());
 app.disable('x-powered-by');
@@ -15,7 +16,7 @@ app.get('/api/lookup/:id', async (request, response) => {
   const { id } = request.params;
   const user = await fetch(`https://fortniteapi.io/v1/lookup?username=${id}`, {
     headers: {
-      Authorization: 'e63f4351-b625ddac-254af606-5a2d8ef0',
+      Authorization: `${API_KEY}`,
     },
   });
   const userData = await user.json();
@@ -35,7 +36,7 @@ app.get('/api/stats', async (request, response) => {
       `https://fortniteapi.io/v1/stats?account=${queries}`,
       {
         headers: {
-          Authorization: 'e63f4351-b625ddac-254af606-5a2d8ef0',
+          Authorization: `${API_KEY}`,
         },
       }
     );
@@ -50,7 +51,7 @@ app.get('/api/stats', async (request, response) => {
     queries.map(async (id) => {
       const res = await fetch(`https://fortniteapi.io/v1/stats?account=${id}`, {
         headers: {
-          Authorization: 'e63f4351-b625ddac-254af606-5a2d8ef0',
+          Authorization: `${API_KEY}`,
         },
       });
       const data = await res.json();
