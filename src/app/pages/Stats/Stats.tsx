d8ece3ed3479dba6fdffb3442e9a2fbb37d96dcd
ChristianSeelemann <cseelemann@gmx.de>
2021-09-22 11:49:24 +0200

    🐛 Complete stats age initialization

--- src/app/pages/Stats/Stats.tsx
+++ src/app/pages/Stats/Stats.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './Stats.module.css';
 
 export default function Stats(): JSX.Element {
-  return <div>Stats</div>;
+  return <section className={styles.stats}>Stats</section>;
 }
