85207d60a99b8b457c3df93594d960783fedf980
ChristianSeelemann <cseelemann@gmx.de>
2021-09-27 11:05:31 +0200

    ✨ Add filter for news

--- src/app/pages/News/News.tsx
+++ src/app/pages/News/News.tsx
@@ -6,6 +6,7 @@ import ToTop from '../../components/Icons/ToTop';
 import Navigation from '../../components/Navigation/Navigation';
 import styles from './News.module.css';
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner';
+import Tag from '../../components/Tag/Tag';
 
 interface newsFromAPI {
   adspace: string;
@@ -21,6 +22,8 @@ interface newsFromAPI {
 
 export default function News(): JSX.Element {
   const [showToTop, setShowToTop] = useState(false);
+  const [isFiltered, setIsFiltered] = useState(false);
+  const [filteredMode, setFilteredMode] = useState('');
 
   const { data: battleRoyaleNews, isLoading: battleRoyaleLoading } =
     useFetch<newsFromAPI[]>('/api/news/?mode=br');
@@ -32,6 +35,11 @@ export default function News(): JSX.Element {
   const { data: saveTheWorldNews, isLoading: saveTheWorldLoading } = useFetch<
     newsFromAPI[]
   >('/api/news/?mode=stw');
+
+  function handleFilterClick(mode: string) {
+    setIsFiltered(true);
+    setFilteredMode(mode);
+  }
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -48,19 +56,45 @@ export default function News(): JSX.Element {
       <section className={styles.news}>
         <Header textThin="Fort" textBold="Knights" icon="fortnite" />
         <main>
+          <section className={styles.news__tags}>
+            <Tag
+              style="battleroyale"
+              text="Battle Royale"
+              onClick={() => handleFilterClick('battleroyale')}
+            />
+            <Tag
+              style="savetheworld"
+              text="Save the World"
+              onClick={() => handleFilterClick('savetheworld')}
+            />
+            <Tag
+              style="creative"
+              text="Creative"
+              onClick={() => handleFilterClick('creative')}
+            />
+          </section>
           <section className={styles.news__itemGroup}>
-            <h2 className={styles.news__h2_first}>Battle Royale</h2>
-            {battleRoyaleNews?.map((item) => (
-              <Card
-                key={item.id}
-                title={item.body}
-                image={item.image}
-                date={item.title}
-                gamemode="br"
-              />
-            ))}
-            <h2 className={styles.news__h2}>Save the World</h2>
+            {battleRoyaleNews &&
+              (!isFiltered || filteredMode === 'battleroyale') && (
+                <h2>Battle Royale</h2>
+              )}
+            {battleRoyaleNews &&
+              (!isFiltered || filteredMode === 'battleroyale') &&
+              battleRoyaleNews?.map((item) => (
+                <Card
+                  key={item.id}
+                  title={item.body}
+                  image={item.image}
+                  date={item.title}
+                  gamemode="br"
+                />
+              ))}
             {saveTheWorldNews &&
+              (!isFiltered || filteredMode === 'savetheworld') && (
+                <h2>Save the World</h2>
+              )}
+            {saveTheWorldNews &&
+              (!isFiltered || filteredMode === 'savetheworld') &&
               saveTheWorldNews.map((item) => (
                 <Card
                   key={item.title}
@@ -70,16 +104,20 @@ export default function News(): JSX.Element {
                   gamemode="stw"
                 />
               ))}
-            <h2 className={styles.news__h2}>Creative</h2>
-            {creativeNews?.map((item) => (
-              <Card
-                key={item.id}
-                title={item.body}
-                image={item.image}
-                date={item.title}
-                gamemode="creative"
-              />
-            ))}
+            {creativeNews && (!isFiltered || filteredMode === 'creative') && (
+              <h2>Creative</h2>
+            )}
+            {creativeNews &&
+              (!isFiltered || filteredMode === 'creative') &&
+              creativeNews?.map((item) => (
+                <Card
+                  key={item.id}
+                  title={item.body}
+                  image={item.image}
+                  date={item.title}
+                  gamemode="creative"
+                />
+              ))}
           </section>
           {showToTop === true && (
             <div className={styles.news__toDo}>
